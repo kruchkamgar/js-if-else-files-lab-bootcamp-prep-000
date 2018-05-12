2e4d90d9445ee13ac84e604ba8c116901d1a66e9
Kurosh Ruch-Kamgar <kruchkamgar1@gmail.com>
2018-05-12 21:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var name = "Joe"
 var height = "74"
 
 
+if (name === "Susan") { console.log("The name appears correct");
+} else {
+  console.log("Expected the name \"Susan\"");
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
