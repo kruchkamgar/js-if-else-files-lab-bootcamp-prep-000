ebec0010a723d0c37303c6c584546aef6cd1c7a4
Kurosh Ruch-Kamgar <kruchkamgar1@gmail.com>
2018-05-12 22:00:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var height = "74"
 if (name === "Susan") { console.log("The name appears correct");
 } else {
   console.log("Expected the name \"Susan\"");
+  name = "Susan";
+  height = "70";
 }
 
 // Don't worry about this
